918a8f69802b0c63d03e0ed4658c26a5bdb1186b
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-07-07 17:05:13 -0300

    add func exibiNoticias + revamp pegaNoticia + styles

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
-const contentPage = document.querySelectorAll('#news')
+const contentPage = document.querySelector('#news')
+let noticias = []
 
-contentPage.forEach( element => {
-    let titulo = element.children['noticia'].children['title'];
-    let image = element.children['noticia'].children['image'];
-    let author = element.children['noticia'].children['author'];
-    let description = element.children['noticia'].children['description'];
-    let content = element.children['noticia'].children['content'];
-    let url = element.children['noticia']
-    pegaNoticia(titulo, image, author, description, content, url)
-
-})
-
-async function pegaNoticia(titulo, image, author, description, content, url) {
+async function pegaNoticia() {
     try {
         const noticia = await fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=7ae7da9b241b4dc4a4e1e901ac5931a0');
-        const noticiaJson = await noticia.json();
-            titulo.innerHTML = noticiaJson.articles[1].title
-            image.src = noticiaJson.articles[1].urlToImage
-            author.innerHTML = noticiaJson.articles[1].author
-            description.innerHTML = noticiaJson.articles[1].description
-            content.innerHTML = noticiaJson.articles[1].content
-            url.href = noticiaJson.articles[1].url
+        noticias = await noticia.json();
+        exibirNoticias(noticias.articles)
     } catch (erro) {
         console.log(erro)
     }
 }
+
+function exibirNoticias(noticias) {
+    noticias.forEach(element => {
+        contentPage.innerHTML += `
+        <div id="news" class="artigo">
+        <a id="noticia" class="info__noticia" target="_blank" href="${element.url}">
+            <h1 id="title" class="info__title">${element.title}</h1>
+            <img id="image" class="info__ImagemTop" src="${element.urlToImage}" alt="test">
+            <h2 id="author" class="info__author">${element.author}</h2>
+            <h3 id="description" class="info__description">${element.description}</h3>
+            <h4 id="publishedAt" class="info__publishedAt">${element.publishedAt}</h4>
+            <p id="content" class="info__content">${element.content}</p>
+        </a>
+    </div>
+        `
+    });
+}
+
+pegaNoticia()
